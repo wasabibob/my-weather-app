a8ffc7fa86086fa8e7b2f938dec8943ed43db1c6
wasabibob <twilmer@pm.me>
2025-02-06 14:21:01 -0700

    improved LD Feature Flag

--- server.py
+++ server.py
@@ -24,7 +24,6 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-
 @app.route('/weather')
 def get_weather():
     city = request.args.get('city')
@@ -33,52 +32,11 @@ def get_weather():
     if not bool(city.strip()):
         # You could render "City Not Found" instead like we do below
         city = "Denver"
-
-    weather_data = get_current_weather(city)
-
-    # City is not found by API
-    if not weather_data['cod'] == 200:
-        return render_template('city-not-found.html')
-
-    return render_template(
-        "weather-plus.html",
-        title=weather_data["name"],
-        status=weather_data["weather"][0]["description"].capitalize(),
-        temp=f"{weather_data['main']['temp']:.1f}",
-        feels_like=f"{weather_data['main']['feels_like']:.1f}"
-    )
-
-def show_evaluation_result(key: str, value: bool):
-    print()
-    print(f"*** The {key} feature flag evaluates to {value}")
-
-    if value:
-        print()
-        print(f"*** The {key} feature flag evaluates to {value}")
-class FlagValueChangeListener:
-    def flag_value_change_listener(self, flag_change):
-        show_evaluation_result(flag_change.key, flag_change.new_value)
-
-if __name__ == "__main__":
-    if not sdk_key:
-        print("*** Please set the LAUNCHDARKLY_SDK_KEY env first")
-        exit()
-    if not feature_flag_key:
-        print("*** Please set the LAUNCHDARKLY_FLAG_KEY env first")
-        exit()
-
-    ldclient.set_config(Config(sdk_key))
-
-    if not ldclient.get().is_initialized():
-        print("*** SDK failed to initialize. Please check your internet connection and SDK credential for any typo.")
-        exit()
-
-    print("*** SDK successfully initialized")
-
+    
     # Set up the evaluation context. This context should appear on your
     # LaunchDarkly contexts dashboard soon after you run the demo.
     context = \
-        Context.builder('example-user-key').kind('user').name('Tony').build()
+        Context.builder('example-user-key').kind('weather-city').name(city).build()
 
     flag_value = ldclient.get().variation(feature_flag_key, context, False)
     show_evaluation_result(feature_flag_key, flag_value)
@@ -88,11 +46,62 @@ if __name__ == "__main__":
         listener = ldclient.get().flag_tracker \
             .add_flag_value_change_listener(feature_flag_key, context, change_listener.flag_value_change_listener)
 
+        
 #        with Halo(text='Waiting for changes', spinner='dots'):
 #            try:
 #                Event().wait()
 #            except KeyboardInterrupt:
 #                pass
+    weather_data = get_current_weather(city)
+
+    # City is not found by API
+    if not weather_data['cod'] == 200:
+        return render_template('city-not-found.html')
+
+    if flag_value is False:
+        return render_template(
+            "weather.html",
+            title=weather_data["name"],
+            status=weather_data["weather"][0]["description"].capitalize(),
+            temp=f"{weather_data['main']['temp']:.1f}"
+        )
+    else:
+        return render_template(
+            "weather-plus.html",
+            title=weather_data["name"],
+            status=weather_data["weather"][0]["description"].capitalize(),
+            temp=f"{weather_data['main']['temp']:.1f}",
+            feels_like=f"{weather_data['main']['feels_like']:.1f}"
+        )
+        
+def show_evaluation_result(key: str, value: bool):
+    print()
+    print(f"*** The {key} feature flag evaluates to {value}")
+
+    if value:
+        print()
+        #print(f"*** The {key} feature flag evaluates to {value}")
+class FlagValueChangeListener:
+    def flag_value_change_listener(self, flag_change):
+        show_evaluation_result(flag_change.key, flag_change.new_value)
+
+if __name__ == "__main__":
+    # Check for the LaunchDarkly SDK key & feature flag key
+    if not sdk_key:
+        print("*** Please set the LAUNCHDARKLY_SDK_KEY env first")
+        exit()
+    if not feature_flag_key:
+        print("*** Please set the LAUNCHDARKLY_FLAG_KEY env first")
+        exit()
+
+    # Initialize the LaunchDarkly SDK
+    ldclient.set_config(Config(sdk_key))
+
+    if not ldclient.get().is_initialized():
+        print("*** SDK failed to initialize. Please check your internet connection and SDK credential for any typo.")
+        exit()
+
+    print("*** SDK successfully initialized")
     
     serve(app, host="0.0.0.0", port=8000)
 #    app.run(host="0.0.0.0", port=8000)
